31f6df647cff662967dc1e8daeb996d5a6b02a25
TigRig <tigrig.crex@gmail.com>
2021-08-09 18:13:51 +0900

    memo を用いたコンポーネント分割

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, {useReducer} from 'react';
+import React, { useReducer, memo, Dispatch} from 'react';
 import ReactDOM from 'react-dom';
 
 interface Todo {
@@ -88,94 +88,130 @@ const reducer = (state: State, action: Action): State => {
   }
 }
 
-const App = () => {
-  const [state, dispatch] = useReducer(reducer, inisialState)
-
-  const filteredTodos = state.todos.filter((todo) => {
-    switch(state.filter) {
-      case 'all':
-        return !todo.removed
-      case 'checked':
-        return todo.checked
-      case 'unchecked':
-        return !todo.checked
-      case 'removed':
-        return todo.removed
-      default:
-        return todo
+const Selector: React.VFC<{dispatch: Dispatch<Action>}> = memo(
+  ({dispatch}) => {
+    const handleOnFilter = (value: Filter) => {
+      dispatch({type: 'filter', value})
     }
-  })
 
-  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault()
-    dispatch({type: 'change', value: e.target.value})
-  }
-
-  const handleOnSubmit = (e: React.FormEvent<HTMLFormElement | HTMLInputElement>) => {
-    e.preventDefault()
-    dispatch({type: 'submit'})
-  }
-
-  const handleOnEdit = (id: number, value: string) => {
-    dispatch({type: 'edit', id, value})
-  }
-
-  const handleOnCheck = (id: number, checked: boolean) => {
-    dispatch({type: 'check', id, checked})
-  }
-
-  const handleOnRemove = (id: number) => {
-    dispatch({type: 'remove', id})
-  }
-
-  const handleOnEmpty = () => {
-    dispatch({type: 'empty'})
-  }
-
-  const handleOnFilter = (value: Filter) => {
-    dispatch({type: 'filter', value})
-  }
-
-  return (
-    <div>
+    return (
       <select defaultValue="all" onChange={(e) => {handleOnFilter(e.target.value as Filter)}}>
         <option value="all">すべてのタスク</option>
         <option value="checked">完了したタスク</option>
         <option value="unchecked">未完了のタスク</option>
         <option value="removed">削除済みのタスク</option>
       </select>
-      {
-        state.filter === 'removed'
-        ? <button
-            disabled={state.todos.filter((todo) => todo.removed).length === 0}
-            onClick={() => handleOnEmpty()}>ゴミ箱を空にする
-          </button>
-        : <form onSubmit={(e) => handleOnSubmit(e)}>
-          <input
-            disabled={state.filter === 'checked'}
-            type="text"
-            value={state.text}
-            onChange={(e) => handleOnChange(e)}
-          />
-          <input
-            disabled={state.filter === 'checked'}
-            type="submit"
-            value="追加"
-            onSubmit={(e) => handleOnSubmit(e)}
-          />
-        </form>
+    )
+  }
+)
+Selector.displayName = 'Selector'
+
+const EmptyButton: React.VFC<{ dispatch: Dispatch<Action> }> = memo(
+  ({dispatch}) => {
+    const handleOnEmpty = () => {
+      dispatch({type: 'empty'})
+    }
+    return (
+      <button className="empty" onClick={handleOnEmpty}>
+        ゴミ箱を空にする
+      </button>
+    )
+  }
+)
+EmptyButton.displayName = 'EmptyButton'
+
+const Form: React.VFC<{ state: State; dispatch: Dispatch<Action> }> = memo(
+  ({state, dispatch}) => {
+    const handleOnSubmit = (e: React.FormEvent<HTMLFormElement | HTMLInputElement>) => {
+      e.preventDefault()
+      dispatch({type: 'submit'})
+    }
+    
+    const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      e.preventDefault()
+      dispatch({type: 'change', value: e.target.value})
+    }
+
+    return (
+      <form className="form" onSubmit={handleOnSubmit}>
+        <input
+          className="text"
+          disabled={state.filter === 'checked'}
+          type="text"
+          value={state.text}
+          onChange={handleOnChange}
+        />
+        <input
+          className="button"
+          disabled={state.filter === 'checked'}
+          type="submit"
+          value="追加"
+          onSubmit={handleOnSubmit}
+        />
+      </form>
+    )
+  }
+)
+Form.displayName = 'Form'
+
+const FilteredTodos: React.VFC<{ state: State, dispatch: Dispatch<Action> }> = memo(
+  ({state, dispatch}) => {
+    const handleOnEdit = (id: number, value: string) => {
+      dispatch({type: 'edit', id, value})
+    }
+  
+    const handleOnCheck = (id: number, checked: boolean) => {
+      dispatch({type: 'check', id, checked})
+    }
+  
+    const handleOnRemove = (id: number) => {
+      dispatch({type: 'remove', id})
+    }
+
+    const filteredTodos = state.todos.filter((todo) => {
+      switch(state.filter) {
+        case 'all':
+          return !todo.removed
+        case 'checked':
+          return todo.checked
+        case 'unchecked':
+          return !todo.checked
+        case 'removed':
+          return todo.removed
+        default:
+          return todo
       }
+    })
+  
+    return (
       <ul>
         {filteredTodos.map((todo) => {
           return (
             <li key={todo.id}>
               <input disabled={todo.removed} type="checkbox" checked={todo.checked} onChange={(e) => handleOnCheck(todo.id, e.target.checked)}/>
-              <input disabled={todo.checked || todo.removed} type="text" value={todo.value} onChange={(e) => handleOnEdit(todo.id, e.target.value)} />
-              <button onClick={() => handleOnRemove(todo.id)}>{todo.removed ? '復元' : '削除'}</button>
+              <input className="text" disabled={todo.checked || todo.removed} type="text" value={todo.value} onChange={(e) => handleOnEdit(todo.id, e.target.value)} />
+              <button className="button" onClick={() => handleOnRemove(todo.id)}>{todo.removed ? '復元' : '削除'}</button>
             </li>
           )
         })}
       </ul>
+    )
+  }
+)
+FilteredTodos.displayName = 'FilteredTodos'
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, inisialState)
+
+  return (
+    <div>
+      <Selector dispatch={dispatch} />
+      {state.filter === 'removed' ? (
+        <EmptyButton dispatch={dispatch}/>
+      ) : (
+        <Form state={state} dispatch={dispatch}/>
+      )}
+      <FilteredTodos state={state} dispatch={dispatch} />
     </div>
   )
 }
