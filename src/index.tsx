67ea3012a994f15b1f07db25e5c94a580a6ab94a
TigRig <tigrig.crex@gmail.com>
2021-08-09 11:19:34 +0900

    入力された文字列を state 管理する仕組み

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+  const [text, setText] = useState('')
+
   return (
     <div>
       <form onSubmit={(e) => e.preventDefault()}>
-        <input type="text" value={''} onChange={(e) => e.preventDefault()} />
+        <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
         <input type="submit" value="追加" onSubmit={(e) => e.preventDefault()} />
       </form>
     </div>
